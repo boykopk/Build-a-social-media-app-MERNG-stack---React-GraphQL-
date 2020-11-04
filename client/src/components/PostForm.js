bfea2692ab00be809eced071ab174db86c09d281
Boyko Katranchev <32291114+boykopk@users.noreply.github.com>
2020-11-04 15:50:37 +0200

    feat: add like button

--- client/src/components/PostForm.js
+++ client/src/components/PostForm.js
@@ -8,19 +8,23 @@ import { FETCH_POSTS_QUERY } from '../util/graphql';
 
 function PostForm() {
   const { values, onChange, onSubmit } = useForm(createPostCallback, {
-    body: ''
+    body: '',
   });
 
   const [createPost, { error }] = useMutation(CREATE_POST_MUTATION, {
     variables: values,
     update(proxy, result) {
       const data = proxy.readQuery({
-        query: FETCH_POSTS_QUERY,
+        query: FETCH_POSTS_QUERY
       });
       data.getPosts = [result.data.createPost, ...data.getPosts];
-      proxy.writeQuery({ query: FETCH_POSTS_QUERY, data });
+      proxy.writeQuery({ query: FETCH_POSTS_QUERY,
+        data: {
+          getPosts: [result.data.createPost, ...data.getPosts]
+        }
+      });
       values.body = '';
-    },
+    }
   });
 
   function createPostCallback() {
@@ -28,20 +32,30 @@ function PostForm() {
   }
 
   return (
-    <Form onSubmit={onSubmit}>
-      <h2>Create a post:</h2>
-      <Form.Field>
-        <Form.Input
-          placeholder='Hi World!'
-          name='body'
-          onChange={onChange}
-          value={values.body}
-        />
-        <Button type='submit' color='teal'>
-          Submit
-        </Button>
-      </Form.Field>
-    </Form>
+    <>
+      <Form onSubmit={onSubmit}>
+        <h2>Create a post:</h2>
+        <Form.Field>
+          <Form.Input
+            placeholder='Hi World!'
+            name='body'
+            onChange={onChange}
+            value={values.body}
+            error={error ? true : false}
+          />
+          <Button type='submit' color='teal'>
+            Submit
+          </Button>
+        </Form.Field>
+      </Form>
+      {error && (
+        <div className='ui error message' style={{ marginBottom: 20 }}>
+          <ul className='list'>
+            <li>{error.graphQLErrors[0].message}</li>
+          </ul>
+        </div>
+      )}
+    </>
   );
 }
 
