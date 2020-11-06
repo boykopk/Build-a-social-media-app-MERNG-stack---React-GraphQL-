4cc6591fa0eff4a78acc2d59a6ffe57ae8f62f3e
Boyko Katranchev <32291114+boykopk@users.noreply.github.com>
2020-11-06 19:34:03 +0200

    fix: delete post

--- client/src/components/DeleteButton.js
+++ client/src/components/DeleteButton.js
@@ -12,23 +12,24 @@ function DeleteButton({ postId, commentId, callback }) {
   const mutation = commentId ? DELETE_COMMENT_MUTATION : DELETE_POST_MUTATION;
 
   const [deletePostOrMutation] = useMutation(mutation, {
+    variables: {
+      postId,
+      commentId
+    },
     update(proxy) {
       setConfirmOpen(false);
       if (!commentId) {
         const data = proxy.readQuery({
           query: FETCH_POSTS_QUERY,
         });
-        data.getPosts = data.getPosts.filter((p) => p.id !== postId);
-        proxy.writeQuery({ query: FETCH_POSTS_QUERY, data });
+        const newPosts = data.getPosts.filter((p) => p.id !== postId);
+        proxy.writeQuery({ query: FETCH_POSTS_QUERY, data: {getPosts: newPosts} });
       }
       if (callback) {
         callback();
       }
     },
-    variables: {
-      postId,
-      commentId
-    },
+    
   });
 
   return (
