9beadda93c7673ee82c872618358dcf524a98aca
Boyko Katranchev <32291114+boykopk@users.noreply.github.com>
2020-12-01 09:09:52 +0200

    final fix

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const { MONGODB } = require("./config.js");
 
 const pubsub = new PubSub();
 
+const PORT = process.env.PORT || 5000;
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -21,9 +23,11 @@ mongoose
   .then(() => {
     console.log("MongoDB Connected!");
     return server.listen({
-      port: 5000,
+      port: PORT
     });
   })
   .then((res) => {
     console.log(`Server running at ${res.url}`);
+  }).catch(err => {
+    console.error(err);
   });
